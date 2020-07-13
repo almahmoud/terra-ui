fa05ab02ba0b1822201476652454829fb7e15f3a
Brian Reilly <breilly@broadinstitute.org>
2020-07-13 13:45:50 -0400

    add real text

--- src/components/PolicyReminderModal.js
+++ src/components/PolicyReminderModal.js
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import { div, h } from 'react-hyperscript-helpers'
 import { ButtonPrimary } from 'src/components/common'
 import { icon } from 'src/components/icons'
@@ -5,9 +6,11 @@ import Modal from 'src/components/Modal'
 import colors from 'src/libs/colors'
 
 
+const bioDataCatalystPolicyReminder = "You are transferring data through the BioData Catalyst security boundary. Downloading controlled-access, individual-level  data through BioData Catalyst in prohibited and downloading other types of data is strongly discouraged, due to the sensitive nature of the data hosted on the platform. Please see the Permissible and Prohibited Data Download section of the Data Upload and Download Policy  for more information. Additionally, transferring data may or may not be supported by your Data Use Agreement(s), Limitation(s), or your Institutional Review Board policies and guidelines. As a BioData Catalyst user, you are solely responsible for adhering to the terms of these policies."
+
 const PolicyReminderModal = ({ onDismiss, onSuccess }) => {
   return h(Modal, {
-    title: 'Policy Reminder',
+    title: h(Fragment, ['Policy Reminder', icon('error-standard', { size: 16, style: { color: colors.warning(), marginLeft: '0.5rem' } })]),
     showCancel: true,
     onDismiss,
     okButton: h(ButtonPrimary, {
@@ -17,9 +20,8 @@ const PolicyReminderModal = ({ onDismiss, onSuccess }) => {
       }
     }, 'Download')
   }, [
-    div({ style: { color: colors.warning() } }, [
-      icon('error-standard', { size: 16, style: { marginRight: '0.5rem' } }),
-      'This is the NEW modal text.'
+    div({ style: {  } }, [
+      bioDataCatalystPolicyReminder
     ])
   ])
 }
